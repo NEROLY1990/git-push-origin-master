461fbd721576ca007e2c2984d941dab64fea0cb7
LY QUACH <lyllq.quach@gmail.com>
2022-02-02 18:22:30 +0100

    rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
+    @ask
   end
+
+  def answer
+    @ask = params[:ask]
+    if @ask.ends_with("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
